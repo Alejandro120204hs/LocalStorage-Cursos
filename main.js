2a877d9d6d0346fe6f1d19eec8c60b2d3a43a724
Alejandro Hernandez <alejandrohernandezsierra233@gmail.com>
2025-09-25 14:01:52 -0500

    Agregue la funcion de crear curssy mostrarlos al recargar

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
-const formulario = document.getElementById('formulario')
-const iNombre = document.getElementById('nombre');
-const saludo = document.getElementById('saludo');
-const btnBorrar = document.getElementById('borrar');
+const formCurso = document.getElementById('formulario-curso');
+const iCurso = document.getElementById('curso');
+const iProfesor = document.getElementById('profesor');
+const iPrecio = document.getElementById('precio');
+const iCiudad = document.getElementById('ciudad');
+const iCupo = document.getElementById('cupo');
+const mensaje = document.getElementById('mensajeCurso');
+const btnCurso = document.getElementById('borrarCurso');
 
-
-formulario.addEventListener('submit',(e)=>{
+formCurso.addEventListener('submit',(e)=>{
 
     e.preventDefault();
-    const nombre = iNombre.value.trim();
-    localStorage.setItem('usuario',nombre);
+    const curso = iCurso.value.trim();
+    const profesor = iProfesor.value.trim();
+    const precio = iPrecio.value.trim();
+    const ciudad = iCiudad.value.trim();
+    const cupo = iCupo.value.trim();
 
-    if(nombre == ''){
-        alert('Ingrese un nombre');
+    if(iCurso == '' || iProfesor == '' || iPrecio == '' || iCiudad == '' || iCupo == ''){
+        
+        alert("Ingrese un valor");
         return;
     }
-    saludo.textContent = 'Bienvenido ' + nombre;
-    iNombre.value='';
+
+    // ESTAMOS ALIMENTANDO UN OBJETO CON SUS DIFERENTES CLAVES Y VALORES
+    const nuevoCurso = {
+        nombre:curso,
+        profesor:profesor,
+        precio:precio,
+        ciudad:ciudad,
+        cupo:cupo
+    }
+    localStorage.setItem('curso', JSON.stringify(nuevoCurso));
+    formCurso.reset();
 });
 
 document.addEventListener('DOMContentLoaded',()=>{
 
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if(usuarioGuardado){
-        saludo.textContent = 'Bienvenido de nuevo ' + usuarioGuardado;
+    const cursoCreado = localStorage.getItem('curso');
+    if(cursoCreado){
+        const objetoCurso = JSON.parse(cursoCreado) //JSON.parse SE USA EN JAVASCRIPT PARA CONVERITR UNA CADENA DE TEXTO EN FORMATO JSON A UN OBBJETO
+        mensaje.innerHTML = 'curso: ' + objetoCurso.nombre + '<br>' + 'profesor: ' + objetoCurso.profesor + '<br>' + 'precio: ' + objetoCurso.precio + '<br>' + 'ciudad: ' + objetoCurso.ciudad  + '<br>' + 'cupo: ' + objetoCurso.cupo;
     }
-    
-
 });
-
-btnBorrar.addEventListener('click',()=>{
-
-    localStorage.removeItem('usuario');
-    saludo.textContent = 'Bienvenido Usuario Indefinido';
-})
-
--- perfil.js
+++
@@ -1,9 +0,0 @@
-const iUsario = document.getElementById('usuarioCreado');
-
-
-document.addEventListener('DOMContentLoaded',()=>{
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if(usuarioGuardado){
-        iUsario.textContent = 'Bienvenido de nuevo ' + usuarioGuardado;
-    }
-})
